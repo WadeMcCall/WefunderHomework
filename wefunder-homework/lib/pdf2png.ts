22e85171381f62c5eb99a8f71d88d8eeeed93490
WadeMcCall <danceifuwantto@gmail.com>
2022-02-10 11:15:31 -0600

    Move env variables into config file and use a variable for the ghostscript command

--- wefunder-homework/lib/pdf2png.ts
+++ wefunder-homework/lib/pdf2png.ts
@@ -10,7 +10,7 @@ const convertToImages = async () => {
     const outputFile = process.env.UPLOAD_PATH + "images\\%d.png"
 
     // The ghostscript command is different for linux and windows
-    const ghostscriptCommand = 'gswin32.exe' // 'gs' on linux; 
+    const ghostscriptCommand = process.env.GHOSTSCRIPT_COMMAND // 'gs' on linux; 
     
     exec(
       `${ghostscriptCommand} -q -dQUIET -dSAFER -dBATCH -dNOPAUSE -dNOPROMPT -dMaxBitmap=500000000 -dAlignToPixels=0 -dGridFitTT=2 -sDEVICE=png16m -dTextAlphaBits=4 -dGraphicsAlphaBits=4 -r${300} -dFirstPage=${1} -dLastPage=${20} -sOutputFile=${outputFile} ${inputFile}`,
--- wefunder-homework/next.config.js
+++ wefunder-homework/next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env:{
+    UPLOAD_PATH:"C:\\Users\\dance\\OneDrive\\Desktop\\React\\Wefunder\\WefunderHomework\\wefunder-homework\\public\\",
+    GHOSTSCRIPT_COMMAND:"gswin32.exe" // 'gs' on linux
+  }
 }
 
 module.exports = nextConfig
